23326e382fb9fafab39156c2bfb7b9159a457c4b
MRSgkce <mursidegokce985@gmail.com>
2024-08-12 12:37:54 +0300

    Bir yemek kataloğu oluşturuldu ve tableView ile herbirinin ismi index mantığıyla yazdırıldı.

--- catalogApp/ViewController.swift
+++ catalogApp/ViewController.swift
@@ -7,13 +7,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
+{
+    var yemekKatalogu=[String]()
+    var yemekGorselIsimleri=[String]()
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate=self
+        tableView.dataSource=self
+      
+        yemekKatalogu.append("musakka")
+        yemekKatalogu.append("pilav")
+        yemekKatalogu.append("corba")
+        yemekKatalogu.append("tatllı")
+        
+        //var yemekGorselleri=[UIImage]()
+        
+        //yemekGorselleri.append(UIImage(named: "musakka")!)
+       
+        yemekGorselIsimleri.append("musakka")
+        yemekGorselIsimleri.append("pilav")
+        yemekGorselIsimleri.append("corba")
+        yemekGorselIsimleri.append("tatllı")
+        
     }
-
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return yemekKatalogu.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text=yemekGorselIsimleri[indexPath.row]
+        return cell
+    }
 
 }
 
